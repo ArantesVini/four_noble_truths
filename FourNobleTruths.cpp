ba26acb82550f170175519f60c92b95927554d36
Vinicius Arantes <67487618+ArantesVini@users.noreply.github.com>
2023-11-29 10:09:55 -0300

    Update FourNobleTruths.cpp to use the hpp file

--- FourNobleTruths.cpp
+++ FourNobleTruths.cpp
@@ -1,30 +1,16 @@
+#include "FourNobleTruths.hpp"
 #include <iostream>
-#include <string>
 
-class FourNobleTruths {
-public:
-    std::string dukkha;
-    std::string samudaya;
-    std::string nirodha;
-    std::string magga;
+FourNobleTruths::FourNobleTruths() {
+    dukkha = "The truth of suffering";
+    samudaya = "The truth of the origin of suffering";
+    nirodha = "The truth of the cessation of suffering";
+    magga = "The truth of the path to the cessation of suffering";
+}
 
-    FourNobleTruths() {
-        dukkha = "The truth of suffering";
-        samudaya = "The truth of the origin of suffering";
-        nirodha = "The truth of the cessation of suffering";
-        magga = "The truth of the path to the cessation of suffering";
-    }
-
-    void printTruths() {
-        std::cout << "Dukkha: " << dukkha << std::endl;
-        std::cout << "Samudaya: " << samudaya << std::endl;
-        std::cout << "Nirodha: " << nirodha << std::endl;
-        std::cout << "Magga: " << magga << std::endl;
-    }
-};
-
-int main() {
-    FourNobleTruths truths;
-    truths.printTruths();
-    return 0;
+void FourNobleTruths::printTruths() {
+    std::cout << "Dukkha: " << dukkha << std::endl;
+    std::cout << "Samudaya: " << samudaya << std::endl;
+    std::cout << "Nirodha: " << nirodha << std::endl;
+    std::cout << "Magga: " << magga << std::endl;
 }
