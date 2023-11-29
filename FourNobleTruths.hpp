77504ebfb4038112c96e7808fd7c007569745c63
Vinicius Arantes <67487618+ArantesVini@users.noreply.github.com>
2023-11-29 10:09:43 -0300

    Create FourNobleTruths.hpp

---
+++ FourNobleTruths.hpp
@@ -0,0 +1,12 @@
+#include <string>
+
+class FourNobleTruths {
+public:
+    std::string dukkha;
+    std::string samudaya;
+    std::string nirodha;
+    std::string magga;
+
+    FourNobleTruths();
+    void printTruths();
+};
